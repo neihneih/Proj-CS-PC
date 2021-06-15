cde202b95d67a5a098b04374dba4fd99bdde2a44
NeihNeih <hoang-hien.nguyen@cpe.fr>
2021-06-15 19:30:54 +0200

    v2

--- Gestion restaurant/resto.py
+++ Gestion restaurant/resto.py
@@ -21,6 +21,7 @@ CL_BLACK = "\033[22;30m"                  #Noir
 
 
 #-------------------------------------------------------------
+
 def effacer_ecran() : 
     print(CLEARSCR,end='')
 
@@ -40,17 +41,17 @@ def couleur_txt(Coul) :
     print(Coul,end='')
 
 
-def indice_to_coord(X,nb_colonnes):
-    colonne = X % nb_colonnes
-    ligne = X // nb_colonnes
+def indice_to_coord(pI,nb_colonnes):
+    colonne = pI % nb_colonnes
+    ligne = pI // nb_colonnes
     return [ligne, colonne]
 
 
-def changer(liste, elem1, elem2):
+def changer(liste, pA, pB):
     for i in range(len(liste)//2):
         if liste[2*i] == -1:
-            liste[2*i] = elem1
-            liste[2*i+1] = elem2
+            liste[2*i] = pA
+            liste[2*i+1] = pB
             return liste
     return liste
 
@@ -58,11 +59,11 @@ def changer(liste, elem1, elem2):
 def prendre(liste):
     for i in range(len(liste)//2):
         if liste[2*i] != -1:
-            idx = liste[2*i]
-            menu = liste[2*i+1]
+            pX = liste[2*i]
+            menu = liste[2*i + 1]
             liste[2*i] = -1
-            liste[2*i+1] = -1
-            return [idx, menu]
+            liste[2*i + 1] = -1
+            return [pX, menu]
     return [-1, -1]
 
 
@@ -73,73 +74,75 @@ def listePleine(liste):
     return True
 
 
-def horloge(a,resto_ouvert,mutexResto, ligneinfo):
-    heureDepart = 9
-    minuteDepart = 00
-    heure = "09"
-    minute = "00"
+def horloge(a,ouverture,mutex_resto, label_info):
+    heure_depart = 7
+    minute_depart = 45
+    heure = "07"
+    minute = "45"
 
     while True:
-        if heureDepart < 10:
-            heure = "0"+str(heureDepart)
+        if heure_depart < 10:
+            heure = "0" + str(heure_depart)
 
         else:
-            heure = str(heureDepart)
+            heure = str(heure_depart)
 
-        if minuteDepart < 10:
-            minute = "0"+str(minuteDepart)
+        if minute_depart < 10:
+            minute = "0" + str(minute_depart)
 
         else:
-            minute = str(minuteDepart)
+            minute = str(minute_depart)
 
         move_to(1, 10)
-        print(heure,":",minute)
-        minuteDepart +=1
+        print("Il est ", heure, "h", minute)
 
-        if minuteDepart == 60:
-            minuteDepart = 0
-            heureDepart += 1
+        minute_depart += 1
 
-        if heureDepart == 24:
-            heureDepart = 0
+        if minute_depart == 60:
+            minute_depart = 0
+            heure_depart += 1
+
+        if heure_depart == 24:
+            heure_depart = 0
 
         time.sleep(0.1)
-        mutexResto.acquire()
 
-        if int(heure) >= 8 and int(heure)<22:
-            resto_ouvert.value = 1
-            move_to(ligneinfo+1, 40)
-            print("O U V E R T")
+        mutex_resto.acquire()
+        if int(heure) >= 8 and int(heure) < 22:
+            ouverture.value = 1
+
+            move_to(label_info + 1, 40)
+            print("NOUS SOMMES OUVERTS")
 
         else:
-            resto_ouvert.value = 0
-            move_to(ligneinfo+1, 40)
-            print("F E R M E  ")
+            ouverture.value = 0
 
-        mutexResto.release()
+            move_to(label_info + 1, 40)
+            print("NOUS SOMMES FERMES ")
+        mutex_resto.release()
 
 
-def processus_client(resto_ouvert,liste, mutex_commandes):
+def processus_client(ouverture,liste, mutex_commandes):
     time.sleep(2)
 
     while True:
-        while resto_ouvert.value == 1:
+        while ouverture.value == 1:
+
             mutex_commandes.acquire()
             listeFull = listePleine(liste)
 
             if not listeFull:
                 liste = changer(liste, random.randint(1000000,9000000), random.randint(0,25))
-
             mutex_commandes.release()
 
-            time.sleep(random.randint(2,10)*0.1)
+            time.sleep(random.randint(2,10)*0.2)
 
 
-def processus_serveur(resto_ouvert,id_serveur, mutex_serveurs,mutex_commandes, liste_serveurs,liste_commandesfinies,mutex_finies):
+def processus_serveur(ouverture,id_serveur, mutex_serveurs,mutex_commandes, liste_serveurs,liste_commandesfinies,mutex_finies):
     while True:
         idxServ = id_serveur - 1
 
-        while resto_ouvert.value == 1:
+        while ouverture.value == 1:
 
             mutex_finies.acquire()
             liste_commandesfinies[3*idxServ+1] = -1
@@ -147,11 +150,11 @@ def processus_serveur(resto_ouvert,id_serveur, mutex_serveurs,mutex_commandes, l
             mutex_finies.release()
 
             mutex_commandes.acquire()
-            [idx, menu] = prendre(liste_commandes)
+            [pX, menu] = prendre(liste_commandes)
 
-            if idx != -1:
+            if pX != -1:
                 mutex_serveurs.acquire()
-                liste_serveurs[3*idxServ+1] = idx
+                liste_serveurs[3*idxServ+1] = pX
                 liste_serveurs[3*idxServ+2] = menu
                 mutex_serveurs.release()
 
@@ -160,96 +163,107 @@ def processus_serveur(resto_ouvert,id_serveur, mutex_serveurs,mutex_commandes, l
                 liste_serveurs[3*idxServ+1] = -1
                 liste_serveurs[3*idxServ+2] = -1
                 mutex_serveurs.release()
-
             mutex_commandes.release()
+
 
             time.sleep(random.randint(1,id_serveur+3))
 
-            if idx != -1:
+            if pX != -1:
                 mutex_finies.acquire()
-                liste_commandesfinies[3*idxServ+1] = idx
+                liste_commandesfinies[3*idxServ+1] = pX
                 liste_commandesfinies[3*idxServ+2] = menu
                 mutex_finies.release()
 
             time.sleep(1)
 
     
-def employes(resto_ouvert,liste_commandes, mutex_commandes, liste_serveurs, mutex_serveurs, nbmax_commandes, nb_serveurs,liste_commandesfinies,mutex_finies):
+def major_dHomme(ouverture,liste_commandes, mutex_commandes, liste_serveurs, mutex_serveurs, nbmax_commandes, nb_serveurs,liste_commandesfinies,mutex_finies):
+    
     while True:
-#Interface
         for i in range(len(liste_commandes)//(2*nbmax_commandes//5)):
-            move_to(i+2*nb_serveurs, 100)
+            move_to(i + 2*nb_serveurs, 100)
             efface_ligne_curseur()
 
         for i in range(len(liste_serveurs)//3):
-            move_to(i+2, 100)
+            move_to(i + 2, 100)
             efface_ligne_curseur()
-            move_to(i+2, 1)
+
+            move_to(i + 2, 1)
             id_serveur = liste_serveurs[3*i]
-            print("Le serveur "+str(id_serveur)+" prepare la commande :")
+            print("Le serveur " + str(id_serveur) + " prepare la commande :")
 
         for i in range(len(liste_commandesfinies)//3):
-            move_to(i+2+20, 100)
+            move_to(i + 2 + 20, 100)
             efface_ligne_curseur()
-            move_to(i+20+2, 1)
+
+            move_to(i + 20 + 2, 1)
             id_serveur = liste_serveurs[3*i]
-            print("Le serveur "+str(id_serveur)+" a donné la commande :")
+            print("Le serveur " + str(id_serveur) + " a servi la commande :")
 
         time.sleep(1)
 
-        while resto_ouvert.value == 1:
+        while ouverture.value == 1:
             mutex_commandes.acquire()
             mutex_serveurs.acquire()
             mutex_finies.acquire()
 
-    #COMMANDES
-        #Efface le tableau des commande en attente
+
+            # PARTIE AFFICHAGE DES COMMANDES
+            # RESET tableau de commandes 
             for i in range(len(liste_commandes)//(2*nbmax_commandes//5)):
-                move_to(i+2*nb_serveurs, 100)
+                move_to(i + 2*nb_serveurs, 100)
                 efface_ligne_curseur()
 
-        #Ecrit le nouveau tableau des commandes en attente
+
+            # Ecriture du tableau de commandes 
             for i in range(len(liste_commandes)//2):
                 if liste_commandes[2*i] != -1:
                     [ligne, colonne] = indice_to_coord(i, 5)
+                    
                     move_to(ligne + 2*nb_serveurs, 15*colonne)
                     idClient = liste_commandes[2*i]
-                    menu = liste_commandes[2*i+1]
-                    print("("+str(idClient)+","+chr(ord('A')+int(menu))+")")
+                    menu = liste_commandes[2*i + 1]
+                    print("(" + str(idClient) + "," + chr(ord('A') + int(menu)) + ")")
 
-    #SERVEURS
+
+            # PARTIE AFFICHAGE DES SERVEURS
             for i in range(len(liste_serveurs)//3):
-                move_to(i+2, 100)
+
+                move_to(i + 2, 100)
                 efface_ligne_curseur()
-                move_to(i+2, 1)
+                
+                move_to(i + 2, 1)
                 id_serveur = liste_serveurs[3*i]
-                print("Le serveur "+str(id_serveur)+" prepare la commande :")
+                print("Le serveur " + str(id_serveur) + " prepare la commande :")
 
             for i in range(len(liste_serveurs)//3):
-                if liste_serveurs[3*i+1] != -1:
-                    ligne = i+2
+                if liste_serveurs[3*i + 1] != -1:
+                    ligne = i + 2
                     id_serveur = liste_serveurs[3*i]
-                    idClient = liste_serveurs[3*i+1]
-                    menu = liste_serveurs[3*i+2]
-                    text = "Le serveur "+str(id_serveur)+" prepare la commande : ("+str(idClient)+","+chr(ord('A')+int(menu))+")"
+                    idClient = liste_serveurs[3*i + 1]
+                    menu = liste_serveurs[3*i + 2]
+                    text = "Le serveur " + str(id_serveur) + " prepare la commande : (" + str(idClient) + "," + chr(ord('A') + int(menu)) + ")"
+                    
                     move_to(ligne, 1)
                     print(text)
 
-    #COMMANDES SERVIES
+
+            # PARTIE AFFICHAGE DES COMMANDES SERVIES
             for i in range(len(liste_commandesfinies)//3):
-                move_to(i+2+20, 100)
+                move_to(i + 2 + 20, 100)
                 efface_ligne_curseur()
-                move_to(i+20+2, 1)
+
+                move_to(i + 20 + 2, 1)
                 id_serveur = liste_serveurs[3*i]
-                print("Le serveur "+str(id_serveur)+" a donné la commande :")
+                print("Le serveur " + str(id_serveur) + " a servi la commande :")
 
             for i in range(len(liste_commandesfinies)//3):
-                if liste_commandesfinies[3*i+1] != -1:
-                    ligne = i+2+20
+                if liste_commandesfinies[3*i + 1] != -1:
+                    ligne = i + 2 + 20
                     id_serveur = liste_commandesfinies[3*i]
-                    idClient = liste_commandesfinies[3*i+1]
-                    menu = liste_commandesfinies[3*i+2]
-                    text = "Le serveur "+str(id_serveur)+" a donné la commande : ("+str(idClient)+","+chr(ord('A')+int(menu))+")"
+                    idClient = liste_commandesfinies[3*i + 1]
+                    menu = liste_commandesfinies[3*i + 2]
+                    text = "Le serveur " + str(id_serveur) + " a servi la commande : (" + str(idClient) + "," + chr(ord('A') + int(menu)) + ")"
                     move_to(ligne, 1)
                     print(text)
 
@@ -261,7 +275,7 @@ def employes(resto_ouvert,liste_commandes, mutex_commandes, liste_serveurs, mute
 
 if __name__ == "__main__" :
     effacer_ecran()
-    resto_ouvert = mp.Value('i', 0)
+    ouverture = mp.Value('i', 0)
     nbmax_commandes = 50
     liste_commandes = mp.Array('i', [-1 for i in range(2*nbmax_commandes)])
     nb_serveurs = 5
@@ -269,43 +283,46 @@ if __name__ == "__main__" :
     liste_commandesfinies = mp.Array('i', [-1 for i in range(3*nb_serveurs)])
 
     for i in range(nb_serveurs):
-        liste_serveurs[3*i] = i+1
-        liste_commandesfinies[3*i] = i+1
+        liste_serveurs[3*i] = i + 1
+        liste_commandesfinies[3*i] = i + 1
 
     liste_process = []
 
     mutex_serveurs = mp.Lock()
     mutex_commandes = mp.Lock()
     mutex_finies = mp.Lock()
-    mutexResto = mp.Lock()
+    mutex_resto = mp.Lock()
 
     #Affichage UI
 
     move_to(nb_serveurs + 3, 1)
     print("COMMANDES EN ATTENTE :")
-    ligneinfo = (nbmax_commandes//5)+nb_serveurs*2+8
-    move_to((nbmax_commandes//5)+nb_serveurs*2+8, 1)
-    print(" ---------------------------------")
-    print(" | Restaurant ouvert de 8h à 22h |")
-    print(" ---------------------------------")
+    label_info = (nbmax_commandes//5) + nb_serveurs*2 + 8
+
+    move_to((nbmax_commandes//5) + nb_serveurs*2 + 8, 1)
+    print(" ====================== ")
+    print(" | Service de 8h à 22h |")
+    print(" ====================== ")
+
     time.sleep(1)
-    ProcessHorloge = mp.Process(target = horloge, args=(0,resto_ouvert,mutexResto, ligneinfo))
+
+    ProcessHorloge = mp.Process(target = horloge, args = (0,ouverture,mutex_resto, label_info))
     liste_process.append(ProcessHorloge)
 
     for i in range(nb_serveurs):
         i += 1
-        p = mp.Process(target=processus_serveur, args=(resto_ouvert,i, mutex_serveurs,mutex_commandes, liste_serveurs,liste_commandesfinies,mutex_finies))
-        liste_process.append(p)
+        p_serv = mp.Process(target = processus_serveur, args = (ouverture,i, mutex_serveurs,mutex_commandes, liste_serveurs,liste_commandesfinies,mutex_finies))
+        liste_process.append(p_serv)
 
-    p1 = mp.Process(target=processus_client, args=(resto_ouvert,liste_commandes, mutex_commandes))
-    liste_process.append(p1)
+    p_client = mp.Process(target = processus_client, args = (ouverture,liste_commandes, mutex_commandes))
+    liste_process.append(p_client)
 
-    p2 = mp.Process(target=employes, args=(resto_ouvert,liste_commandes, mutex_commandes, liste_serveurs, mutex_serveurs,nbmax_commandes, nb_serveurs,liste_commandesfinies,mutex_finies))
-    liste_process.append(p2)
+    p_affichage = mp.Process(target = major_dHomme, args = (ouverture,liste_commandes, mutex_commandes, liste_serveurs, mutex_serveurs,nbmax_commandes, nb_serveurs,liste_commandesfinies,mutex_finies))
+    liste_process.append(p_affichage)
 
 
-    for p in liste_process:    
-        p.start()
+    for p_serv in liste_process:    
+        p_serv.start()
 
-    for p in liste_process:    
-        p.join()
+    for p_serv in liste_process:    
+        p_serv.join()
